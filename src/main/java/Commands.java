5d53bf59174c37bf657575cc2d185500a79a8e7c
dfalfaro <diego.alfaro@tlgcohort.com>
2023-03-16 13:17:52 -0400

    Added player class. Attempted to use Gson to read Json file

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,4 +1,15 @@
+import java.util.List;
+import java.util.Map;
+
 public class Commands {
+    private Player player;
+    private static Location currentLocation;
+
+    public Commands(Player player, Location currentLocation) {
+        this.player = player;
+        this.currentLocation = currentLocation;
+    }
+
     public void parseCommand(String command) {
         String[] words = command.split(" ");
         String verb = words[0].toLowerCase();
@@ -12,7 +23,16 @@ public class Commands {
             case "look":
                 // handle "look" command
                 if (noun.equals("")) {
-                    System.out.println("You look around but see nothing of interest.");
+                    System.out.println(currentLocation.getDescription());
+                    List<String> items = currentLocation.getItems();
+                    if (!items.isEmpty()) {
+                        System.out.println("You see the following items:");
+                        for (String item : items) {
+                            System.out.println("- " + item);
+                        }
+                    } else {
+                        System.out.println("You don't see any items here.");
+                    }
                 } else {
                     System.out.println("You look at the " + noun + ".");
                     // handle looking at specific object
@@ -24,9 +44,18 @@ public class Commands {
                 if (noun.equals("")) {
                     System.out.println("Go where?");
                 } else {
-                    System.out.println("You go to the " + noun + ".");
-                    // handle going to specific location
-
+                    Map<String, String> directions = currentLocation.getDirections();
+                    if (directions.containsKey(noun)) {
+                        String nextLocationName = directions.get(noun);
+                        for (Location location : Main.locations) {
+                            if (location.getName().equals(nextLocationName)) {
+                                currentLocation = location;
+                                player.setLocation(currentLocation);
+                            }
+                        }
+                    } else {
+                        System.out.println("You can't go in that direction.");
+                    }
                 }
                 break;
 
@@ -35,5 +64,4 @@ public class Commands {
                 break;
         }
     }
-
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,28 @@
+import com.google.gson.Gson;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
+    private static Player player;
+    private static Location currentLocation;
+    static Location[] locations;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(System.in);
-        Commands parser = new Commands();
+
+        Gson gson = new Gson();
+        FileReader reader = new FileReader("src\\main\\java\\locations.json");
+        locations = gson.fromJson(reader, Location[].class);
+
+
+        // set current location to the first location in the array
+        currentLocation = locations[0];
+
+        // create a new player with starting values
+        player = new Player("Player", 100, currentLocation);
+
+        Commands parser = new Commands(player, currentLocation);
 
         // Display game title
         System.out.println("Welcome!");
@@ -31,7 +49,8 @@ public class Main {
             while (true) {
 
                 // Display status (player name, health, lives, inventory)
-
+                System.out.println("Location: " + currentLocation.getName());
+                System.out.println();
 
                 System.out.println("What would you like to do?");
                 System.out.print("> ");
