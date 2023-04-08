da769aea698e6c631443b39c8c752abcbd5a9a69
Nyckolle Lucuab <nlucuab18@gmail.com>
2023-04-07 17:16:20 -0700

    Adding in unit tests for GameIntroPanel, AB#758 (#53)
    * Added splash art, and display logic WIP, AB#758
    
    * Added start screen & info screen splash art, AB#804, AB#758
    
    * setting up unit tests, AB#758
    
    * add in GameIntroPanel Unit Tests, AB#758

---
+++ src/test/java/com/questforholygrail/game/ui/GameIntroPanelTest.java
@@ -0,0 +1,57 @@
+package com.questforholygrail.game.ui;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameIntroPanelTest {
+
+    GameIntroPanel gip;
+    JLabel instructionArtImageLabel;
+    ImageIcon instructionArtImageIcon;
+
+    public final ImageIcon TEST_ICON = new ImageIcon(Objects.requireNonNull(getClass().getResource("/splash-Art/Instructions-SplashArt.png")));
+    public final ImageIcon NEW_ICON = new ImageIcon(Objects.requireNonNull(getClass().getResource("/splash-Art/Grail-SplashArt.png")));
+    public final JLabel TEST_LABEL = new JLabel(TEST_ICON);
+    public final JLabel NEW_LABEL = new JLabel(NEW_ICON);
+
+
+
+    @BeforeEach
+    public void beforeEach() {
+        gip = new GameIntroPanel();
+        instructionArtImageIcon = TEST_ICON;
+        instructionArtImageLabel = TEST_LABEL;
+        instructionArtImageLabel.setSize(968,567);
+        gip.add(instructionArtImageLabel);
+    }
+
+    @Test
+    void testGetImgLabel_notNewLabel() {
+        assertNotEquals(gip.getImgLabel(), NEW_LABEL);
+    }
+
+    @Test
+    void testSetImgLabel_newLabel() {
+        gip.setImgLabel(NEW_LABEL);
+        assertEquals(gip.getImgLabel(), NEW_LABEL);
+    }
+
+    @Test
+    void testGetImgIcon_notNewIcon(){
+        assertNotEquals(gip.getImgIcon(), NEW_ICON);
+    }
+
+    @Test
+    void testSetImgIcon_newIcon() {
+        gip.setImgIcon(NEW_ICON);
+        assertEquals(gip.getImgIcon(), NEW_ICON);
+    }
+}
